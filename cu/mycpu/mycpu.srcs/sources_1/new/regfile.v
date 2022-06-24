5eef771984f4381556ab6aac27fdf30b434786f0
wufisher <1105889424@qq.com>
2022-06-24 16:13:42 +0800

    drwa

--- cu/mycpu/mycpu.srcs/sources_1/new/regfile.v
+++ cu/mycpu/mycpu.srcs/sources_1/new/regfile.v
@@ -11,6 +11,17 @@ module regfile(
     input[31:0] wdata
 
 );
+reg[31:0] reg_array[31:0];
 
+always @(posedge clk) begin
+    if(we)
+    begin
+      reg_array[waddr] <= wdata;
+    end
+    
+end
+
+assign rdata1 = (raddr1 == 0) ? 32'b0: reg_array[raddr1];
+assign rdata2 = (raddr2 == 0) ? 32'b0: reg_array[raddr2];
 
 endmodule
