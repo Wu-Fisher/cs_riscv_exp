ccac23361634526dc76367279eca7866419771a2
wufisher <1105889424@qq.com>
2022-06-24 14:46:57 +0800

    project

---
+++ cu/mycpu/mycpu.ip_user_files/sim_scripts/cpuclk/ies/run.f
@@ -0,0 +1,14 @@
+-makelib ies_lib/xil_defaultlib -sv \
+  "/home/wufisher/Xilinx/Vivado/2018.3/data/ip/xpm/xpm_cdc/hdl/xpm_cdc.sv" \
+-endlib
+-makelib ies_lib/xpm \
+  "/home/wufisher/Xilinx/Vivado/2018.3/data/ip/xpm/xpm_VCOMP.vhd" \
+-endlib
+-makelib ies_lib/xil_defaultlib \
+  "../../../../mycpu.srcs/sources_1/ip/cpuclk/cpuclk_clk_wiz.v" \
+  "../../../../mycpu.srcs/sources_1/ip/cpuclk/cpuclk.v" \
+-endlib
+-makelib ies_lib/xil_defaultlib \
+  glbl.v
+-endlib
+
---
+++ cu/mycpu/mycpu.srcs/sources_1/new/regfile.v
@@ -0,0 +1,16 @@
+module regfile(
+    input clk,
+    input[4:0] raddr1,
+    input[4:0] raddr2,
+
+    output[4:0] rdata1,
+    output[4:0] rdata2,
+
+    input we,
+    input[4:0] waddr,
+    input[31:0] wdata
+
+);
+
+
+endmodule
